0885c41b9f5faedaa94932439333934e2965d939
Lalith Krishna <lalithkrishna2212@gmail.com>
2026-03-24 18:58:48 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://taskmaster-assignment-smartinterv.vercel.app',
+  origin: process.env.CLIENT_URL || 'https://taskmaster-assignment-smartinterviews.vercel.app',
   credentials: true
 }));
 app.use(express.json());
